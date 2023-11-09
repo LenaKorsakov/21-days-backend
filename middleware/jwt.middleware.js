064a7c30fe5c0b150b2ef6fe54c668ea58d4baf1
ElenaKorsakova <lenawwka@gmail.com>
2023-11-09 15:53:43 +0100

    Fix: authorization

--- middleware/jwt.middleware.js
+++ middleware/jwt.middleware.js
@@ -1,26 +1,22 @@
-const { expressjwt: jwt } = require("express-jwt");
+const jwt = require("jsonwebtoken")
 
-// Instantiate the JWT token validation middleware
-const isAuthenticated = jwt({
-  secret: process.env.TOKEN_SECRET,
-  algorithms: ["HS256"],
-  requestProperty: "payload",
-  getToken: getTokenFromHeaders,
-});
+const isAuthenticated = async (req, res, next) => {
+	try {
+		let token = req.headers.authorization
+		if (!token) {
+			return res.status(401).json({ message: "No token found in the headers" })
+		}
+		token = token.replace("Bearer ", "")
 
-// Function used to extract the JWT token from the request's 'Authorization' Headers
-function getTokenFromHeaders(req) {
-  // Check if the token is available on the request Headers
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.split(" ")[0] === "Bearer"
-  ) {
-    // Get the encoded token string and return it
-    const token = req.headers.authorization.split(" ")[1];
-    return token;
-  }
+		const payload = jwt.verify(token, process.env.TOKEN_SECRET, {
+			algorithms: ["HS256"],
+		})
 
-  return null;
+		req.userId = payload._id
+		next()
+	} catch (error) {
+		next(error)
+	}
 }
 
 // Export the middleware so that we can use it to create protected routes
